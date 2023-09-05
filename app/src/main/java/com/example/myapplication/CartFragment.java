e64632ad8da98e838bc3516355f2a05677247c7f
dwdwb <pipal1221@gmail.com>
2023-09-05 18:34:20 +0900

    feat: cart to order nav 추가

--- app/src/main/java/com/example/myapplication/CartFragment.java
+++ app/src/main/java/com/example/myapplication/CartFragment.java
@@ -3,17 +3,34 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CartFragment extends Fragment {
+import com.example.myapplication.databinding.FragmentCartBinding;
+import com.example.myapplication.databinding.FragmentDetailBinding;
 
+public class CartFragment extends Fragment {
+    private static final String TAG = "CartFragment";
+    private FragmentCartBinding binding;
+    private NavController navController;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCartBinding.inflate(inflater);
+
+        navController = NavHostFragment.findNavController(this);
+
+        initBtnOrder();
+
+        return binding.getRoot();
+    }
+
+    private void initBtnOrder() {
+        binding.btnOrder.setOnClickListener(v -> {
+            navController.navigate(R.id.action_cart_to_order);
+        });
     }
 }
