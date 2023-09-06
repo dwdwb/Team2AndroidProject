6432e9a4b38e6fe0feec262e5f97f9707063f57e
dwdwb <pipal1221@gmail.com>
2023-09-06 20:57:55 +0900

    feat: 상단바, 하단바 구현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,66 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private NavController navController;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host);
         navController = navHostFragment.getNavController();
+
+        initAppBar();
+
+        initDestinationChangedListener();
+    }
+
+    private void initAppBar() {
+        //Toolbar를 Appbar로 설정
+        setSupportActionBar(binding.toolbar);
+        //그래프 상에 있는 이동 경로를 참고
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //Toolbar의 타이틀 및 탐색 버튼(목록 또는 백버튼)을 navController 탐색 그래프를 참고해서 처리
+        // 1) Toolbar의 제목은 대상의 label 속성을 이용해서 표시됨
+        // 2) 백버튼(<-)이 표시되며, 클릭시 이전 대상으로 이동
+
+        //Toolbar만 단독으로 사용할 경우
+        NavigationUI.setupWithNavController(binding.toolbar,navController, appBarConfiguration);
+
+        //CollapsingToolbarLayout을 사용할 경우
+        /*NavigationUI.setupWithNavController(binding.collapsingToolbarLayout, binding.toolbar,navController, appBarConfiguration);*/
+
+    }
+
+    private void initDestinationChangedListener() {
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                //이동한 대상에 따른 처리
+                if(navDestination.getId() == R.id.detail) {
+                    getSupportActionBar().hide();
+                } else if (navDestination.getId() == R.id.main) {
+                    getSupportActionBar().show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/ui/DetailFragment.java
+++ app/src/main/java/com/example/myapplication/ui/DetailFragment.java
@@ -2,18 +2,26 @@ package com.example.myapplication.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDetailBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DetailFragment extends Fragment {
     private static final String TAG = "DetailFragment";
@@ -24,6 +32,9 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentDetailBinding.inflate(inflater);
 
+        //NavController 얻기
+        navController = NavHostFragment.findNavController(this);
+
         initBtnAddCart();
         initBtnOrder();
         initBtnWriteInquiry();
@@ -31,7 +42,45 @@ public class DetailFragment extends Fragment {
         initBtnCart();
         initBtnBack();
 
+        //메뉴 초기화
+        initMenu();
+
+        //하단바 숨기기
+        hideBottomNavigation(true);
+
         return binding.getRoot();
+    }
+
+    //하단바 숨기기
+    public void hideBottomNavigation(Boolean bool) {
+        BottomNavigationView bottomNavigation = getActivity().findViewById(R.id.bottomNavigationView);
+        if (bool == true)
+            bottomNavigation.setVisibility(View.GONE);
+        else
+            bottomNavigation.setVisibility(View.VISIBLE);
+    }
+
+    private void initMenu() {
+        MenuProvider menuProvider = new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.top_menu,menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.main) {
+                    navController.popBackStack(R.id.main, false);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.cart) {
+                    navController.navigate(R.id.action_detail_to_cart);
+                    return true;
+                }
+                return false;
+            }
+        };
+
+        getActivity().addMenuProvider(menuProvider, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
     }
 
     private void initBtnAddCart() {
@@ -68,5 +117,13 @@ public class DetailFragment extends Fragment {
         binding.btnBack.setOnClickListener(v -> {
             navController.popBackStack();
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //하단바 보이기
+        hideBottomNavigation(false);
     }
 }
