b24e6d8cdf24f631693c1a74d28435a3124232e0
dwdwb <pipal1221@gmail.com>
2023-09-13 16:33:31 +0900

    feature: 상품상세 수정

--- app/src/main/java/com/example/myapplication/ui/InquiryFragment.java
+++ app/src/main/java/com/example/myapplication/ui/InquiryFragment.java
@@ -24,42 +24,6 @@ public class InquiryFragment extends Fragment {
 
         navController = NavHostFragment.findNavController(this);
 
-        initBtnMyPage();
-        initBtnSearch();
-        initBtnMain();
-        initBtnCart();
-        initBtnBack();
-
         return binding.getRoot();
-    }
-
-    private void initBtnMain() {
-        binding.btnMain.setOnClickListener(v -> {
-            navController.popBackStack(R.id.main, false);
-        });
-    }
-
-    private void initBtnSearch() {
-        binding.btnList.setOnClickListener(v -> {
-            navController.navigate(R.id.action_inquiry_to_search);
-        });
-    }
-
-    private void initBtnCart() {
-        binding.btnCart.setOnClickListener(v -> {
-            navController.navigate(R.id.action_inquiry_to_cart);
-        });
-    }
-
-    private void initBtnMyPage() {
-        binding.btnMyPage.setOnClickListener(v -> {
-            navController.popBackStack();
-        });
-    }
-
-    private void initBtnBack() {
-        binding.btnBack.setOnClickListener(v -> {
-            navController.popBackStack();
-        });
     }
 }
