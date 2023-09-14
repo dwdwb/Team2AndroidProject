ff907157e9a31cf04c42796da3978715fce7dbd1
noismik <www_lol@naver.com>
2023-09-14 14:20:53 +0900

    feat: 마이페이지 배송지관리 불러오기

--- app/src/main/java/com/example/myapplication/ui/ShippingFragment.java
+++ app/src/main/java/com/example/myapplication/ui/ShippingFragment.java
@@ -5,14 +5,26 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapter.AddressAdapter;
 import com.example.myapplication.databinding.FragmentOrderHistoryBinding;
 import com.example.myapplication.databinding.FragmentShippingBinding;
+import com.example.myapplication.dto.AddressList;
+import com.example.myapplication.service.AddressBookService;
+import com.example.myapplication.service.ServiceProvider;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ShippingFragment extends Fragment {
     private static final String TAG = "ShippingFragment";
@@ -25,13 +37,58 @@ public class ShippingFragment extends Fragment {
         navController = NavHostFragment.findNavController(this);
 
         initBtnAddShipping();
-        initBtnMyPage();
-        initBtnSearch();
-        initBtnMain();
-        initBtnCart();
-        initBtnBack();
+        initRecyclerView();
 
         return binding.getRoot();
+    }
+
+    private void initRecyclerView() {
+        //RecyclerView에서 항목을 수직으로 배치하도록 설정
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                getContext(), LinearLayoutManager.VERTICAL, false
+        );
+        binding.recyclerView
+                .setLayoutManager(linearLayoutManager);
+
+        //어댑터 생성
+        AddressAdapter addressAdapter = new AddressAdapter();
+        //Log.i(TAG, "size: " + addressAdapter.getItemCount());
+
+        //API 서버에서 JSON 목록 받기
+        AddressBookService addressBookService = ServiceProvider.getAddressBookService(getContext());
+        Call<List<AddressList>> call = addressBookService.getAddressList();
+        call.enqueue(new Callback<List<AddressList>>() {
+            @Override
+            public void onResponse(Call<List<AddressList>> call, Response<List<AddressList>> response) {
+                List<AddressList> list = response.body();
+
+                // onResponse 콜백 내에서 어댑터에 데이터 설정 및 리사이클러뷰에 연결
+                addressAdapter.setList(list);
+                binding.recyclerView.setAdapter(addressAdapter);
+
+                Log.i(TAG, "size: " + addressAdapter.getItemCount());
+            }
+
+            @Override
+            public void onFailure(Call<List<AddressList>> call, Throwable t) {
+                Log.i(TAG, "나너무슬퍼");
+                t.printStackTrace();
+            }
+        });
+
+        //항목을 클릭했을 때 콜백 객체를 등록
+        addressAdapter.setOnItemClickListener(new AddressAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int position) {
+                Log.i(TAG, position + "번 항목 클릭됨");
+                AddressList addressList = addressAdapter.getItem(position);
+
+                Bundle args = new Bundle();
+                args.putSerializable("addressList", addressList);
+
+            }
+        });
+
     }
 
     private void initBtnAddShipping() {
@@ -40,33 +97,5 @@ public class ShippingFragment extends Fragment {
         });
     }
 
-    private void initBtnMyPage() {
-        binding.btnMyPage.setOnClickListener(v -> {
-            navController.popBackStack();
-        });
-    }
 
-    private void initBtnSearch() {
-        binding.btnSearch.setOnClickListener(v -> {
-            navController.navigate(R.id.action_shipping_to_search);
-        });
-    }
-
-    private void initBtnMain() {
-        binding.btnMain.setOnClickListener(v -> {
-            navController.popBackStack(R.id.main, false);
-        });
-    }
-
-    private void initBtnCart() {
-        binding.btnCart.setOnClickListener(v -> {
-            navController.navigate(R.id.action_shipping_to_cart);
-        });
-    }
-
-    private void initBtnBack() {
-        binding.btnBack.setOnClickListener(v -> {
-            navController.popBackStack();
-        });
-    }
 }
