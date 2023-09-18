237a785eb53a6f143b9a8254f0b4ff467232d9fb
noismik <www_lol@naver.com>
2023-09-18 16:27:27 +0900

    feat: 주문내역 검색기능

--- app/src/main/java/com/example/myapplication/service/MyPageOrderedService.java
+++ app/src/main/java/com/example/myapplication/service/MyPageOrderedService.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface MyPageOrderedService {
 
     @GET("getOrderedList")
     Call<List<OrderHistory>> getOrderedList();
+
+    @GET("searchOrderedList")
+    Call<List<OrderHistory>> searchOrderedList(
+            @Query("shopperNo") int shopperNo,
+            @Query("searchKeyword") String searchKeyword
+    );
 }
--- app/src/main/java/com/example/myapplication/ui/OrderHistoryFragment.java
+++ app/src/main/java/com/example/myapplication/ui/OrderHistoryFragment.java
@@ -11,18 +11,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.adapter.AddressAdapter;
 import com.example.myapplication.adapter.MyPageOrderedAdapter;
 import com.example.myapplication.databinding.FragmentListBinding;
 import com.example.myapplication.databinding.FragmentOrderHistoryBinding;
+import com.example.myapplication.datastore.AppKeyValueStore;
 import com.example.myapplication.dto.AddressList;
 import com.example.myapplication.dto.OrderHistory;
+import com.example.myapplication.dto.Shopper;
 import com.example.myapplication.service.AddressBookService;
 import com.example.myapplication.service.ListService;
 import com.example.myapplication.service.MyPageOrderedService;
 import com.example.myapplication.service.ServiceProvider;
+import com.example.myapplication.service.ShopperService;
 
 import java.util.List;
 
@@ -34,17 +38,103 @@ public class OrderHistoryFragment extends Fragment {
     private static final String TAG = "OrderHistoryFragment";
     private FragmentOrderHistoryBinding binding;
     private NavController navController;
+    private SearchView searchView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentOrderHistoryBinding.inflate(inflater);
-        //NavController 얻기
+        binding = FragmentOrderHistoryBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        // NavController 얻기
         navController = NavHostFragment.findNavController(this);
+
+        // XML에서 정의한 SearchView를 참조합니다.
+        searchView = view.findViewById(R.id.searchView);
+
+        // SearchView의 이벤트 리스너를 설정합니다.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String searchKeyword) {
+                // 사용자가 검색 버튼을 누르면 호출됩니다.
+                // 이곳에서 서버로 검색 쿼리를 보내고 결과를 처리합니다.
+                // query 변수에 검색어가 포함됩니다.
+                Log.i(TAG, "나검색어임"+searchKeyword);
+                performSearch(searchKeyword);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // 검색어가 변경될 때마다 호출됩니다.
+                // 이곳에서 검색어를 가지고 동적으로 검색 결과를 업데이트할 수 있습니다.
+                return false;
+            }
+        });
 
         initRecyclerView();
 
-
-        return binding.getRoot();
+        return view;
     }
+
+    private void performSearch(String searchKeyword) {
+        // 서버로 검색 쿼리를 보내고 결과를 처리하는 코드를 작성합니다.
+        // query 변수에 검색어가 포함됩니다.
+        String shopperId = AppKeyValueStore.getValue(getContext(), "shopperId");
+        ShopperService shopperService = ServiceProvider.getShopperService(getContext());
+        Log.i(TAG, "나쇼퍼아이디임"+shopperId);
+        Call<Shopper> call =shopperService.getShopper(shopperId);
+
+
+        call.enqueue(new Callback<Shopper>() {
+            @Override
+            public void onResponse(Call<Shopper> call, Response<Shopper> response) {
+                Shopper shopper = response.body();
+                int shopperNo = shopper.getShopperNo();
+                MyPageOrderedService myPageOrderedService = ServiceProvider.getMyPageOrderedService(getContext());
+                Log.i(TAG, "나검색어임"+searchKeyword);
+                Log.i(TAG, "나쇼퍼넘버임"+shopperNo);
+                Call<List<OrderHistory>> call2 = myPageOrderedService.searchOrderedList(shopperNo, searchKeyword);
+
+                call2.enqueue(new Callback<List<OrderHistory>>() {
+                    @Override
+                    public void onResponse(Call<List<OrderHistory>> call2, Response<List<OrderHistory>> response2) {
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                                getContext(), LinearLayoutManager.VERTICAL, false
+                        );
+                        binding.recyclerView
+                                .setLayoutManager(linearLayoutManager);
+
+                        //어댑터 생성
+                        MyPageOrderedAdapter myPageOrderedAdapter = new MyPageOrderedAdapter();
+
+                        List<OrderHistory> list = response2.body();
+                        Log.i(TAG, "나검색된리스트임"+list.toString());
+                        myPageOrderedAdapter.setList(list);
+                        binding.recyclerView.setAdapter(myPageOrderedAdapter);
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<OrderHistory>> call2, Throwable t2) {
+
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Shopper> call, Throwable t) {
+
+            }
+        });
+
+
+    }
+
+    // 나머지 코드 및 멤버 변수들도 필요에 따라 추가하세요.
+
+
+
 
 
 
