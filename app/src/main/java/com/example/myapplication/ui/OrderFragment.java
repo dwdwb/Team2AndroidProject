7b135cc4d018bdbde00460d6b8bc5e3b7cf9d6e3
noismik <www_lol@naver.com>
2023-09-20 14:22:41 +0900

    feat: 주문결제 form 수정

--- app/src/main/java/com/example/myapplication/ui/OrderFragment.java
+++ app/src/main/java/com/example/myapplication/ui/OrderFragment.java
@@ -1,9 +1,12 @@
 package com.example.myapplication.ui;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 import androidx.navigation.NavController;
@@ -14,8 +17,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.adapter.OrderAdapter;
@@ -41,6 +49,17 @@ public class OrderFragment extends Fragment {
 
     private NavController navController;
 
+    private Button payButtonCard;
+    private Button payButtonAccount;
+    private Button payButtonNoBank;
+    private Button payButtonPhone;
+    private boolean isCardToggled;
+    private boolean isAccountToggled;
+    private boolean isPhoneToggled;
+    private boolean isNoBankToggled;
+    private Spinner spinner;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
     private static final String TAG = "OrderFragment";
 
 
@@ -52,12 +71,47 @@ public class OrderFragment extends Fragment {
         //NavController 얻기
         navController = NavHostFragment.findNavController(this);
 
+        spinner = binding.orderSpinner;
+        arrayList = new ArrayList<>();
+        arrayList.add("휴대폰번호");
+        arrayList.add("현금영수증카드");
+
+        arrayAdapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_spinner_dropdown_item, arrayList);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), arrayList.get(position)+"가 선택 되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        payButtonCard = binding.card;
+        payButtonAccount = binding.account;
+        payButtonNoBank = binding.noBank;
+        payButtonPhone = binding.phone;
+        isCardToggled = false;
+        isAccountToggled = false;
+        isPhoneToggled = false;
+        isNoBankToggled = false;
+
+        initBtnPhone();
+        initBtnNoBank();
+        initBtnCard();
+        initBtnAccount();
         initBtnOrderHistory();
         initBtnOrderShipping();
         initRecyclerView();
 
         Bundle bundle = getArguments();
-        if (bundle != null) {
+        if (bundle.getSerializable("productList") != null) {
             ArrayList<ProductBoard> productList = (ArrayList<ProductBoard>) bundle.getSerializable("productList");
             Log.i(TAG, productList.toString());
 
@@ -79,15 +133,99 @@ public class OrderFragment extends Fragment {
                 binding.delFee.setText(3000+"원");
                 binding.finalPrice.setText(decimalFormat.format(totalPrice+3000)+"원");
             }
-
-
-
-
         }
 
 
         return binding.getRoot();
     }
+
+    private void initBtnAccount() {
+
+        payButtonAccount.setOnClickListener(v -> {
+            if(isCardToggled || isNoBankToggled || isPhoneToggled) {
+                payButtonNoBank.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonNoBank, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonCard.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonCard, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonPhone.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonPhone, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+            }
+            isCardToggled = false;
+            isNoBankToggled = false;
+            isPhoneToggled = false;
+            isAccountToggled = true;
+
+            payButtonAccount.setTextColor(Color.parseColor("#ffffff"));
+            ViewCompat.setBackgroundTintList(payButtonAccount, ColorStateList.valueOf(Color.parseColor("#A0DE5F")));
+
+        });
+    }
+
+    private void initBtnCard() {
+
+        payButtonCard.setOnClickListener(v -> {
+            if(isAccountToggled || isNoBankToggled || isPhoneToggled) {
+                payButtonNoBank.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonNoBank, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonAccount.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonAccount, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonPhone.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonPhone, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+            }
+
+            isCardToggled = true;
+            isNoBankToggled = false;
+            isPhoneToggled = false;
+            isAccountToggled = false;
+
+            payButtonCard.setTextColor(Color.parseColor("#ffffff"));
+            ViewCompat.setBackgroundTintList(payButtonCard, ColorStateList.valueOf(Color.parseColor("#A0DE5F")));
+
+
+        });
+    }
+
+    private void initBtnNoBank() {
+
+        payButtonNoBank.setOnClickListener(v -> {
+            if(isCardToggled || isAccountToggled || isPhoneToggled) {
+                payButtonAccount.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonAccount, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonCard.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonCard, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonPhone.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonPhone, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+            }
+            isCardToggled = false;
+            isNoBankToggled = true;
+            isPhoneToggled = false;
+            isAccountToggled = false;
+            payButtonNoBank.setTextColor(Color.parseColor("#ffffff"));
+            ViewCompat.setBackgroundTintList(payButtonNoBank, ColorStateList.valueOf(Color.parseColor("#A0DE5F")));
+
+        });
+    }
+
+    private void initBtnPhone() {
+        payButtonPhone.setOnClickListener(v -> {
+            if(isCardToggled || isAccountToggled || isNoBankToggled) {
+                payButtonAccount.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonAccount, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonCard.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonCard, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+                payButtonNoBank.setTextColor(Color.parseColor("#A0DE5F"));
+                ViewCompat.setBackgroundTintList(payButtonNoBank, ColorStateList.valueOf(Color.parseColor("#ffffff")));
+            }
+            isCardToggled = false;
+            isNoBankToggled = false;
+            isPhoneToggled = true;
+            isAccountToggled = false;
+            payButtonPhone.setTextColor(Color.parseColor("#ffffff"));
+            ViewCompat.setBackgroundTintList(payButtonPhone, ColorStateList.valueOf(Color.parseColor("#A0DE5F")));
+
+        });
+    }
+
 
     private void initRecyclerView() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
